306ffaf48d22768e010ce7e46b164feeb8d501fb
ozgursertel <sertel.ozgur@gmail.com>
2024-06-05 11:26:42 +0300

    init project

---
+++ src/main/java/com/ozgursertel/booking_list/BookingListApplication.java
@@ -0,0 +1,13 @@
+package com.ozgursertel.booking_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingListApplication.class, args);
+	}
+
+}
